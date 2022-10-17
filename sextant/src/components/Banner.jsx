00542ec08c219d85b747ccc26982f140764323e0
Anmol Pal <anmol111pal@gmail.com>
2022-10-17 21:22:56 +0530

    Create the front-end

---
+++ sextant/src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  
+  const styling= {
+    textWeight: '900',
+    fontSize: '2em'
+  }
+
+  return (
+    <h1 className="text-center p-3" style={styling}> Sextant </h1>
+  )
+}
+
+export default Banner
