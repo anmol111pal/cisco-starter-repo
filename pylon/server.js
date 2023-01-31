69757f2943af27b8a73a241f0dccf17006b662c8
Anmol Pal <anmol111pal@gmail.com>
2023-01-31 22:38:19 +0530

    Made changes to send request to the client with the current timestamp

--- pylon/server.js
+++ pylon/server.js
@@ -6,9 +6,7 @@ server.listen(55455);
 const wsServer = new webSocketServer({ httpServer: server });
 
 wsServer.on('request', function (request) {
-    console.log('establishing a new connection with client');
+    console.log('Establishing a new connection with client');
     var connection = request.accept(null, request.origin);
-    setInterval(() => {
-        connection.sendUTF(new Date().getTime())
-    }, 100);
+    connection.sendUTF(new Date().getTime());
 });
